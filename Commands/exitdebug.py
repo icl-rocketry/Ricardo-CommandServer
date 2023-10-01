40724b29ec2d65dbbd7b12f946ca1a8a9068f5cf
artemsheykin <artem.sheykin20@imperial.ac.uk>
2023-10-01 13:09:07 +0100

    payload cmds

--- Commands/exitdebug.py
+++ Commands/exitdebug.py
@@ -1,12 +1,15 @@
 from CommandServer.commandserver import CommandServer
+from cmd2 import Cmd2ArgumentParser
 import time
 
-@CommandServer.register('exitdebug')
+exitdebug_ap = Cmd2ArgumentParser()
+exitdebug_ap.add_argument("--argument",type=int,required=True)
+@CommandServer.register('exitdebug',argparse=exitdebug_ap)
 def exitdebug(instance,args):
 
     command_packet_args = {"source":1,
                         "source_service":instance.source_service,
-                        "destination":2,
+                        "destination":args['argument'],
                         "destination_service":2,
                         "command_id":106,
                         "command_arg":0}
