29b3a9ba3130107664d4fde84422f61ed24affc5
SC21 <sc3820@ic.ac.uk>
2023-10-12 12:51:10 +0100

    cameras on cmd

---
+++ Commands/cameraon.py
@@ -0,0 +1,15 @@
+from CommandServer.commandserver import CommandServer
+import time
+
+
+@CommandServer.register('cameraon')
+def cameraon(instance,args):
+
+    command_packet_args = {"source":1,
+                        "source_service":instance.source_service,
+                        "destination":14,
+                        "destination_service":2,
+                        "command_id":1,
+                        "command_arg":0}
+
+    instance.send_command_packet(command_packet_args)
