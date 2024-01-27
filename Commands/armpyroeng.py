989230afa50a519a2734278a94a4cc3692f562a9
jak <jackx.crane@hotmail.com>
2024-01-27 14:01:55 +0000

    hotfire - 9/2/24 v1

--- Commands/armpyroeng.py
+++ Commands/armpyroeng.py
@@ -7,7 +7,11 @@ def armpyroeng(instance,args):
     command_packet_args = {"source":1,
                         "source_service":instance.source_service,
                         "destination":107,
+<<<<<<< HEAD
                         "destination_service":12,
+=======
+                        "destination_service":10,
+>>>>>>> 81c1bab (hotfire - 9/2/24 v1)
                         "command_id":3,
                         "command_arg":0}
 
