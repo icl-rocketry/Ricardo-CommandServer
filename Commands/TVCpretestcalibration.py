abc87f01448a06bd70b2915b863d99e1ed44b3b0
Artem <artem.sheykin20@imperial.ac.uk>
2024-02-23 17:12:03 +0000

    added pretest tvc calibration cmd

--- Commands/TVCpretestcalibration.py
+++ Commands/TVCpretestcalibration.py
@@ -2,9 +2,15 @@ from CommandServer.commandserver import CommandServer
 from cmd2 import Cmd2ArgumentParser
 import time
 
+<<<<<<< HEAD
 TVCpretestcalibration_ap = Cmd2ArgumentParser()
 @CommandServer.register('TVCpretestcalibration',argparse=TVCpretestcalibration_ap)
 def TVCpretestcalibration(instance,args):
+=======
+TVCcalibration_ap = Cmd2ArgumentParser()
+@CommandServer.register('TVCcalibration',argparse=TVCcalibration_ap)
+def TVCcalibration(instance,args):
+>>>>>>> 185b00d (added pretest tvc calibration cmd)
 
     command_packet_args = {"source":1,
                         "source_service":instance.source_service,
